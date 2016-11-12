c4e5eb71b2e9a7b31c5eb8be3032f710ab2ac27b
sfillm07 <sfillm07@yahoo.com>
2016-11-11 16:40:11 -0800

    Added IBoutlets

--- BoutTimeGame/ViewController.swift
+++ BoutTimeGame/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var time = 60
+    var timer = Timer()
 
+    @IBOutlet var factLabels: [UILabel]!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var shakeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countdownTimer()
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,7 +28,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func dateFormatter() {
+
+    @IBAction func fullDownButton() {
+    }
+    
+    @IBAction func fullUpButton() {
+    }
+    
+    @IBAction func upHalfTopButton() {
+    }
+    
+    @IBAction func downHalfTopButton() {
+    }
+    
+    @IBAction func upHalfBottomButton() {
+    }
+    
+    @IBAction func downHalfBottomButton() {
+    }
+    
+    func countdownTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+    }
+    
+    func counter() {
+        time -= 1
+        
+        timerLabel.text = "0:\(time)"
+        
+        if time == 0 {
+            timer.invalidate()
+            timerLabel.isHidden = true
+            shakeLabel.isHidden = true
+        }
     }
     
 }
